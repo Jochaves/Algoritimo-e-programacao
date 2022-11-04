475710a39e2a27067a5bbedf046445b3f3ff5fe8
João Chaves <89935861+Jochaves@users.noreply.github.com>
2022-11-04 14:09:57 -0300

    Update NumDivisivel.py

--- Calculo/NumDivisivel.py
+++ Calculo/NumDivisivel.py
@@ -1,9 +1,6 @@
 #O programa deve receber um número inteiro, digitado pelo usuário, e apresentar uma mensagem: 
-
 #- se o número que o usuário digitou for divisível por 3 e por 5 ao mesmo tempo, a mensagem será: O número é divisível por 3 e 5. 
-
 #- se o número que o usuário digitou não for divisível por 3 e por 5 ao mesmo tempo, a mensagem será: O número não é divisível por 3 e 5. 
-
 
 numX = int(input("Qual é o número em mente por você?: "))
 
