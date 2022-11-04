3415abd514dfc43b393c68bf8a5df939cd096678
João Chaves <89935861+Jochaves@users.noreply.github.com>
2022-11-04 14:31:40 -0300

    Delete Calculo directory

--- Calculo/Desconto.py
+++
@@ -1,5 +0,0 @@
-#Faça um algoritimo que leia o preço de um produto e mostre o novo preço, com 15% de desconto.
-
-preço= float(input('Qual é o preço do produto? R$'))
-novo = preço - (preço * 15 / 100)
-print('O produto que custavs R${}, na promoçãocom desconto de 15% vai custar R${}'.format(preço, novo))
