d6101c2cfccaad9785bf62888cd8b6c5bb9a1e09
João Chaves <89935861+Jochaves@users.noreply.github.com>
2022-12-06 18:15:04 -0300

    Add files via upload

---
+++ Cálculos/Imparoupar.py
@@ -0,0 +1,7 @@
+#Escreva um programa em Python que leia um número inteiro e exiba se ele é um número par ou i4mpar.
+
+n1 = float(input("Digite um número: "))
+if n1%2==0:
+    print('%.lf é par. ' %n1)
+else:
+    print('%.lf é impar. ' %n1)
