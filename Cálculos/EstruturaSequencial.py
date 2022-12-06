3ba8cb140a863e5d348f55047620dfc0c9cc1a55
João Chaves <89935861+Jochaves@users.noreply.github.com>
2022-12-06 17:50:10 -0300

    Update EstruturaSequencial.py

--- Cálculos/EstruturaSequencial.py
+++ Cálculos/EstruturaSequencial.py
@@ -1,8 +1,8 @@
 #Escreva um programa em Python que permite ao usuário digitar dois números inteiros e exibir o resultado para cada uma 
 #das seguintes operações, nesta ordem soma, subitração, multiplicação, divisão truncada, resto e exponenciação 
 
-a = int(input())
-b = int(input())
+a = int(input("Digite o primeiro número:"))
+b = int(input("Digite o segundo número:"))
 print (a+b)
 print (a-b)
 print (a*b)
