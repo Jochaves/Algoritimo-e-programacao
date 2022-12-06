dbde22d2d8e90a4778b2ab6f1b7a0f530888b888
João Chaves <89935861+Jochaves@users.noreply.github.com>
2022-12-06 16:32:59 -0300

    Add files via upload

---
+++ Cálculos/NumInteiros.py
@@ -0,0 +1,8 @@
+#Faça um programa em Python que leia dois números inteiros e exiba o quadarado da diferença  do primeiro valor para o segunto.
+
+
+import math
+a = int (input())
+b = int (input())
+res = math.pow(a-b,2)
+print = (res)
