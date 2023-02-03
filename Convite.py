c17a666262dc7eb3e1e72fb3998d434a9877fa87
João Chaves <89935861+Jochaves@users.noreply.github.com>
2023-02-03 15:26:25 -0300

    Update Convite.py

--- Convite.py
+++ Convite.py
@@ -1,11 +1,23 @@
-#Faça um programa em Python que receba o custo (valor em reais) de um espetáculo teatral e o preço do convite (valor em reais) desse espetáculo. Esse programa deve calcular e mostrar: 
-# a) A quantidade de convites que devem ser vendidos para que, pelo menos, o custo do espetáculo seja alcançado. 
-# b) A quantidade de convites que devem ser vendidos para que se tenha um lucro de 23%.  
+#Faça um programa em Python que receba o custo (valor em reais) de um espetáculo teatral e o preço do convite (valor em reais) desse espetáculo. Esse programa deve calcular e mostrar, nesta ordem:
 
- 
-custo_total = float(input("Gasto total do evento: "))
-valor_ingresso  = float(input("Valor de cada ingesso: "))
-quantidade_ingressos = int(custo_total // valor_ingresso)
-lucro = int((custo_total + custo_total * 0.23) // valor_ingresso)
-print("Quantidade de convites que devem ser vendidos para que, pelo menos, o custo do espetáculo seja alcançado: {}".format(quantidade_ingressos))
-print("quantidade de convites que devem ser vendidos para que se tenha um lucro de 23%: {}".format(lucro))
+#a) A quantidade de convites que devem ser vendidos para que pelo menos o custo do espetáculo seja alcançado.
+
+#b) A quantidade de convites que devem ser vendidos para que se tenha um lucro de 23%.
+
+#Observe que as quantidades calculadas a serem vendidas devem ser um número inteiro; portanto, o resultado da quantidade de convites deve ser arredondado para cima, usando a função matemática apropriada em Python.
+
+
+
+import math
+
+#entrada de dados
+custo = float(input("Gasto total do evento: "))
+ingresso  = float(input("Valor de cada ingesso: "))
+
+#Processamento
+qingressos = math.ceil(custo / ingresso)
+lucro = math.ceil((custo + custo *0.23) / ingresso)
+
+#saída 
+print(f"A quantidade de convites que devem ser vendidos para que pelo menos o custo do espetáculo seja alcançado: {qingressos}")
+print(f"A quantidade de convites que devem ser vendidos para ter o lucro de 23% e : {lucro}")
