f2b94562980aaf0a2b09f4fd6391640e61056a46
João Chaves <89935861+Jochaves@users.noreply.github.com>
2023-02-20 23:37:48 -0300

    Update NumDivisivel3e5.py

--- NumDivisivel3e5.py
+++ NumDivisivel3e5.py
@@ -2,13 +2,16 @@
 #- se o número que o usuário digitou for divisível por 3 e por 5 ao mesmo tempo, a mensagem será: O número é divisível por 3 e 5. 
 #- se o número que o usuário digitou não for divisível por 3 e por 5 ao mesmo tempo, a mensagem será: O número não é divisível por 3 e 5. 
 
-
-numX = int(input("Qual é o número em mente por você?: "))
+numX  =  int ( input ( "Qual é o número em mente por você?: " ))
 
 if (numX % 3) == 0 and (numX % 5) == 0:
+    {
+      
 
-   print("O número que você digitou é (",numX,") e sim, ele É divisível por três e cinco ao mesmo tempo.")
-
+   print ( "O número é divisível por 3 e 5" )
+}
 else:
+   {
 
-   print("O número que você digitou é (",numX,") e não, ele NÃO é divisível por três e cinco ao mesmo tempo.")
+   print ( "O número não é divisível por 3 e 5" )
+   }
